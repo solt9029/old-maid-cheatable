71bc6d09a67bdcf4958c61a551a144b7be1b2331
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-03-25 14:42:01 +0900

    Bad code

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,15 @@ import { Card as RCard, CardImg, Col, Container, Row } from "reactstrap";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { backSideImageSrc, Card, initialCards } from "../data/card";
-import { chunk, clone, filter, groupBy, shuffle } from "lodash";
+import {
+  chunk,
+  clone,
+  filter,
+  groupBy,
+  shuffle,
+  uniqBy,
+  uniqueId,
+} from "lodash";
 
 const StyledCard = styled(RCard)`
   max-width: 100px;
@@ -64,12 +72,70 @@ export default function GamePage() {
   const [currentPlayerId, setCurrentPlayerId] = useState(
     initialCurrentPlayerId
   );
+  const [winnerPlayerIds, setWinnerPlayerIds] = useState([]);
 
   const [cards, setCards] = useState(initialState);
 
   useEffect(() => {
-    setInterval(() => {}, 1000);
-  }, []);
+    setTimeout(() => {
+      if (winnerPlayerIds.length >= 3) {
+        return;
+      }
+
+      let clonedCards = clone(cards);
+
+      let nextPlayerId = currentPlayerId;
+
+      while (
+        winnerPlayerIds.includes(nextPlayerId) === true ||
+        nextPlayerId === currentPlayerId
+      ) {
+        nextPlayerId = nextPlayerId + 1;
+        if (nextPlayerId > 3) {
+          nextPlayerId = 0;
+        }
+      }
+
+      if (winnerPlayerIds.includes(currentPlayerId)) {
+        setCurrentPlayerId(nextPlayerId);
+        return;
+      }
+
+      for (let i = 0; i < clonedCards.length; i++) {
+        if (
+          clonedCards[i].playerId === nextPlayerId &&
+          clonedCards[i].isDiscarded === false
+        ) {
+          clonedCards[i].playerId = currentPlayerId;
+          break;
+        }
+      }
+
+      const processedCards = discardCards(clonedCards);
+
+      // 勝者決定ロジック
+      const cardPlayerIds = processedCards.map((card) => {
+        if (card.isDiscarded) {
+          return undefined;
+        }
+        return card.playerId;
+      });
+      const uniqCardPlayerIds = uniqBy(
+        cardPlayerIds,
+        (cardPlayerId) => cardPlayerId
+      );
+      let winPlayerIds = [];
+      for (let i = 0; i < 4; i++) {
+        if (!uniqCardPlayerIds.includes(i)) {
+          winPlayerIds.push(i);
+        }
+      }
+
+      setCards(processedCards);
+      setWinnerPlayerIds(winPlayerIds);
+      setCurrentPlayerId(nextPlayerId);
+    }, 2000);
+  }, [currentPlayerId]);
 
   return (
     <Container>
@@ -79,6 +145,7 @@ export default function GamePage() {
             <Row>
               <h3>
                 {playerId === yourPlayerId ? "あなた" : `プレイヤー${playerId}`}
+                {winnerPlayerIds.includes(playerId) ? "勝利!" : ""}
               </h3>
             </Row>
             <Row>
